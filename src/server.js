b45eff52f432f072ce16d3dbc69307714d64e686
Cesar Gutierrez <cesar.ovidio.gutierrez@gmail.com>
2024-05-09 15:19:17 -0300

    add winston and morgan

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import logger from 'morgan';
 import { router } from './router';
 
 // TODO: add setEnvVariables
 dotenv.config();
 
 const server = express();
+
+server.use(logger(process.env.LOGGER_LEVEL));
 
 server.use('/api', router);
 
