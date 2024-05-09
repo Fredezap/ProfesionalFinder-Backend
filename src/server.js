11973d61422910a1dd093dc64342637101213216
Cesar Gutierrez <cesar.ovidio.gutierrez@gmail.com>
2024-05-09 15:09:38 -0300

    add logger

--- src/server.js
+++ src/server.js
@@ -1,12 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { router } from './router';
-import { connectToDatabase } from './database/connection';
 
 // TODO: add setEnvVariables
 dotenv.config();
-
-connectToDatabase();
 
 const server = express();
 
